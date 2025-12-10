746359f0b5993d1050bfdd059bfe08fc27e2d9e9
ZhaoXiang Lan <tuv12540@ece-000.eng.temple.edu>
2025-12-10 00:31:59 -0500

    heap structure and min-heap

---
+++ DataStructures/heap.py
@@ -0,0 +1,141 @@
+'''
+file: DataStructures/heap.py
+
+ZhaoXiang Lan, 12/4/2025
+
+description:
+The heap data structure implementation.
+
+
+compare_func(a, b):
+    return True if a should be above b
+    return False otherwise
+
+Example:
+    min-heap: h = Heap(lambda a, b: a < b)
+    max-heap: h = Heap(lambda a, b: a > b)
+    or define a function separately and pass it in.
+
+----------------------------------------------------------------------
+'''
+
+# import array
+from DataStructures.array import array
+
+# heap class
+class Heap:
+    # constructor
+    def __init__(self, compare_function):
+        self.data  = array()
+        self.compare = compare_function
+
+    # get the size of the heap
+    def size(self):
+        # return the number of elements in the heap
+        return self.data.size()
+    
+    # return the root element of the heap
+    def peek(self):
+        # return None if heap is empty
+        if self.size() == 0:
+            return None
+        # return the root element
+        return self.data.get(0)
+    
+    # push an element into the heap
+    def push(self, value):
+        # add the element to the end of the array
+        self.data.append(value)
+        # heapify up from the last index
+        self.heapify_up(self.size() - 1)
+
+    # pop the root element from the heap
+    def pop(self):
+        # set the curren size of the heap n
+        n = self.size()
+        # return None if heap is empty
+        if n == 0:
+            return None
+        
+        # get the root element
+        root = self.data.get(0)
+
+        # get the last element
+        last_element = self.data.get(n - 1)
+
+        # remove the last element
+        self.data.set(n-1, None)
+        self.data._size -= 1
+
+        if self.size() > 0:
+            # move last element to root
+            self.data.set(0, last_element)
+            # heapify down from the root
+            self.heapify_down(0)
+
+    # heapify up method
+    def heapify_up(self, index):
+        while index > 0:
+            # get the parrent index
+            parent_index = (index - 1) // 2
+            # get the values
+            child_value = self.data.get(index)
+            # get the parent value
+            parent_value = self.data.get(parent_index)
+
+            # compare child and parent
+            if self.compare(child_value, parent_value):
+            # swap child and parent
+                self.data.set(index, parent_value)
+                self.data.set(parent_index, child_value)
+                # move to parent index
+                index = parent_index
+            else:
+                break
+
+    # heapify down method
+    def heapify_down(self, index):
+        # get the current size of the heap
+        n = self.size()
+
+        while index < n:
+            # get the left and right child indices
+            left_index = 2 * index + 1
+            right_index = 2 * index + 2
+            # assume the current index is the largest/smallest
+            best_index = index
+
+            # get the best value
+            best_value = self.data.get(best_index)
+
+            # compare with left child
+            if left_index < n:
+                # get the left child value
+                left_value = self.data.get(left_index)
+                # compare left child with best value
+                if self.compare(left_value, best_value):
+                    # update best index and value if left child is better
+                    best_index = left_index
+                    best_value = left_value
+
+            # compare with right child
+            if right_index < n:
+                # get the right child value
+                right_value = self.data.get(right_index)
+                # compare right child with best value
+                if self.compare(right_value, best_value):
+                    # update best index and value if right child is better
+                    best_index = right_index
+                    best_value = right_value
+
+            # check if the best index is different from the current index
+            if best_index == index:
+                # heap property is satisfied
+                break
+            else:
+                # swap current index with best index
+                current_value = self.data.get(index)
+                self.data.set(index, best_value)
+                self.data.set(best_index, current_value)
+                # move to best index
+                index = best_index
---
+++ algorithms/heapsort.py
@@ -0,0 +1,69 @@
+'''
+file: DataStructures/heap.py
+
+ZhaoXiang Lan, 12/4/2025
+
+description:
+using the basic heap structure to build both min-heap and max-heap.
+
+Heap data structure description:
+compare_func(a, b):
+    return True if a should be above b
+    return False otherwise
+Example:
+    min-heap: h = Heap(lambda a, b: a < b)
+    max-heap: h = Heap(lambda a, b: a > b)
+    or define a function separately and pass it in.
+----------------------------------------------------------------------
+'''
+# input the root path
+import os
+import sys
+
+# add the project root to sys.path
+PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+if PROJECT_ROOT not in sys.path:
+    sys.path.append(PROJECT_ROOT)
+
+# import Heap
+from DataStructures.heap import Heap
+
+# import array
+from DataStructures.array import array
+
+# min-heap class
+class MinHeap:
+    # constructor
+    def __init__(self):
+        # initialize a min-heap using the Heap class
+        self.heap = Heap(lambda a, b: a < b)
+
+    # insert an new element
+    def push (self, value):
+        self.heap.push(value)
+
+    # remove and return the minimum element
+    def pop(self):
+        return self.heap.pop()
+    
+    # return the minimum element without removing it
+    def peek(self):
+        return self.heap.peek()
+    
+    # return the size of the min-heap
+    def size(self):
+        return self.heap.size()
+    
+    # return whether the min-heap is empty
+    def is_empty(self):
+        return self.size() == 0
+    
+    # return all value
+    def get_all_elements(self):
+        # pop all elements and return them in an array
+        elements = array()
+        size = self.size()
+        for i in range(size):
+            elements.append(self.pop())
+        # return the array of elements
+        return elements
