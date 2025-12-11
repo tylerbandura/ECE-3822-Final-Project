03dc60c430b71b4ba9c317f46832984001351f2e
tylerbandura <tuu03215@temple.edu>
2025-12-11 14:47:22 -0500

    not done yet

---
+++ MyMovieExplorer/query3.py
@@ -0,0 +1,9 @@
+''' 
+file: ECE-3822-Final-Project/MyMovieExplorer/query3.py
+description: The code for query 3, which finds all movies in a certain given genre that released between 2 years using a binary search tree.
+Author: Tyler Bandura
+Date: 12/10/25
+Email: tuu03215@temple.edu
+
+import os
+'''
